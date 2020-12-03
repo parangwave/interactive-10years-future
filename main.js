8ce639ce5f16157b044d66e07b11995561aad893
su <ojs9578@naver.com>
2020-12-03 23:57:50 +0900

    Added main.js code for basic logic
    js로 사이트의 기본동작을 구축함,
    css 에 visible 클래스 추가 등 수정

---
+++ main.js
@@ -0,0 +1,48 @@
+(()=> {
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElems = document.querySelectorAll('.graphic-item');
+    let currentItem = graphicElems[0]; 
+    // 현재 활성화된 (visiㅠle 클래스가 붙은) .graphic-item을 지정
+    let ioIndex;
+
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index * 1;
+    });
+
+    for(let i=0; i < stepElems.length ; i++){
+        io.observe(stepElems[i]);
+        // stepElems[i].setAttribute('.data-index', i);
+        stepElems[i].dataset.index = i
+        graphicElems[i].dataset.index = i
+    }
+
+    function activate(){
+        currentItem.classList.add('visible');
+    }
+
+    function inactivate(){
+        currentItem.classList.remove('visible');
+    }
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+
+        // for (let i = 0; i < stepElems.length; i++) { 
+        // 비효율적으로 매번 10번 도는 loop를 continue를 통해 2,3번만 돌게 만듦
+        for(let i = ioIndex-1; i < ioIndex+2; i++) {
+            step = stepElems[i];
+            if (!step) continue;
+            boundingRect = step.getBoundingClientRect();
+
+            if(boundingRect.top > window.innerHeight* 0.1 && 
+                boundingRect.top < window.innerHeight * 0.8) {
+                    inactivate();
+                    currentItem = graphicElems[step.dataset.index];
+                    activate();
+            }
+        }
+    });
+
+    activate();
+})(); 
