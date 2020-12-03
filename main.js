05a2adef042f99d2cdfb9d6eab16ce3cfdcf68a0
su <ojs9578@naver.com>
2020-12-04 00:12:00 +0900

    Edited css and js
    bird.gif 날아가는 애니메이션 만듦, 새로고침하면 자동으로 페이지 top으로 이동 등

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
 (()=> {
+    
+    const actions = {
+        birdFlies(key){
+            if (key){
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+            }
+        },
+        birdFlies2(key){
+            if (key){
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+            }
+        }
+    };
+
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
     let currentItem = graphicElems[0]; 
-    // 현재 활성화된 (visiㅠle 클래스가 붙은) .graphic-item을 지정
+    // 현재 활성화된 (visible 클래스가 붙은) .graphic-item을 지정
     let ioIndex;
 
     const io = new IntersectionObserver((entries, observer) => {
         ioIndex = entries[0].target.dataset.index * 1;
     });
 
-    for(let i=0; i < stepElems.length ; i++){
+    for(let i = 0; i < stepElems.length ; i++){
         io.observe(stepElems[i]);
         // stepElems[i].setAttribute('.data-index', i);
         stepElems[i].dataset.index = i
         graphicElems[i].dataset.index = i
     }
 
-    function activate(){
+    function activate(action){
         currentItem.classList.add('visible');
+
+        if (action) {
+            actions[action](true);
+        }
     }
 
-    function inactivate(){
+    function inactivate(action){
         currentItem.classList.remove('visible');
+
+        if (action) {
+            actions[action](false);
+        }
     }
 
     window.addEventListener('scroll', () => {
@@ -30,7 +56,7 @@
 
         // for (let i = 0; i < stepElems.length; i++) { 
         // 비효율적으로 매번 10번 도는 loop를 continue를 통해 2,3번만 돌게 만듦
-        for(let i = ioIndex-1; i < ioIndex+2; i++) {
+        for(let i = ioIndex - 1; i < ioIndex + 2; i++) {
             step = stepElems[i];
             if (!step) continue;
             boundingRect = step.getBoundingClientRect();
@@ -39,9 +65,13 @@
                 boundingRect.top < window.innerHeight * 0.8) {
                     inactivate();
                     currentItem = graphicElems[step.dataset.index];
-                    activate();
+                    activate(currentItem.dataset.action);
             }
         }
+    });
+
+    window.addEventListener('load', () => {
+        setTimeout (() => scrollTo(0, 0), 100);
     });
 
     activate();
