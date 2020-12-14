52dc15121d778985ac1683840172ea715799f711
su <ojs9578@naver.com>
2020-12-14 17:27:33 +0900

    Added Images and commented bird-animation
    birdFliesanimation 주석처리, html에서 내용 수정, 이미지 추가

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 (()=> {
     
     const actions = {
-        birdFlies(key){
-            if (key){
-                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
-            } else {
-                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
-            }
-        },
-        birdFlies2(key){
-            if (key){
-                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
-            } else {
-                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
-            }
-        }
+        // birdFlies(key){
+        //     if (key){
+        //         document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+        //     } else {
+        //         document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+        //     }
+        // },
+        // birdFlies2(key){
+        //     if (key){
+        //         document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+        //     } else {
+        //         document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+        //     }
+        // }
     };
 
     const stepElems = document.querySelectorAll('.step');
